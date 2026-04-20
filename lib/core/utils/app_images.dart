2a219e56cb8dc4431e16a06475e7cd6d948cca03
hagar <hajarghonim19@gmail.com>
2026-04-21 01:06:24 +0200

    app images

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAppIcon1
+  /// assets/images/app_icon1.svg
+  static const String imagesAppIcon1 = "assets/images/app_icon1.svg";
+
+  /// Assets for imagesAppIcon2
+  /// assets/images/app_icon2.png
+  static const String imagesAppIcon2 = "assets/images/app_icon2.png";
+
+  /// Assets for imagesOnBoardding3
+  /// assets/images/on_boardding3.svg
+  static const String imagesOnBoardding3 = "assets/images/on_boardding3.svg";
+
+  /// Assets for imagesOnBoarding2
+  /// assets/images/on_boarding2.svg
+  static const String imagesOnBoarding2 = "assets/images/on_boarding2.svg";
+
+  /// Assets for imagesOnboarding2
+  /// assets/images/onboarding2.svg
+  static const String imagesOnboarding2 = "assets/images/onboarding2.svg";
+}
+
